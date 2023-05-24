4fa79b91aacc4de7acb24372c154f7a2c44b1f60
Knumasalina <133725741+Knumasalina@users.noreply.github.com>
2023-05-24 15:24:31 -0500

    Ejercicio número uno
    Para este primer ejercicio tendréis que realizar lo siguiente:
    
    Crea un proyecto de Java desde 0
    
    Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
    
    Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
    
    Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.

---
+++ primeraclase.java
@@ -0,0 +1,18 @@
+public class primeraclase {
+
+    public static void main(String[] args) {
+        int variable1 = 1;
+        long variable2 = 234573;
+        double variable3 = 3.1415;
+        boolean variable4 = true;
+        String name = "Mi nombre";
+
+            System.out.println(variable1);
+            System.out.println(variable2);
+            System.out.println(variable3);
+            System.out.println(variable4);
+            System.out.println(name);
+    }
+    
+    
+}
