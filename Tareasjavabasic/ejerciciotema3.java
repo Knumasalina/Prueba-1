8035c4f087b417c9417b7c8bb08caf625b8b14eb
Knumasalina <133725741+Knumasalina@users.noreply.github.com>
2023-05-25 12:32:48 -0500

    Add files via upload
    En este ejercicio tenéis que crear un bucle que permita concatenar textos e imprima el resultado final por consola.
    
    Tened en cuenta que los textos pueden venir de un array de tipo String. Por ejemplo:
    
    String[] nombres = {"", "", "", ""}

---
+++ Tareasjavabasic/ejerciciotema3.java
@@ -0,0 +1,15 @@
+package Tareasjavabasic;
+
+public class ejerciciotema3 {
+    public static void main(String[] args) {
+        
+        String[] nombre = {"Kevin", "Yaneth", "estudian"};
+
+        for (int i = 3; i == nombre.length; i++){
+            System.out.println(nombre[0] + " y " + nombre[1] + " " + nombre[2]);
+
+        }
+
+
+    }
+}
